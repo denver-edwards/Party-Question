aec923b0e4ae0f4e425924197372b5683ca3720a
Unsignia <csdedwards@gmail.com>
2022-06-24 21:57:10 -0400

    d

---
+++ pages/_offline.js
@@ -0,0 +1,12 @@
+export default function Offline() {
+  return (
+    <div className="absolute align-center justify-center text-center">
+      <h1 className="text-white font-bold ">OFFLINE</h1>
+      <h4>Please check your internet connection</h4>
+
+      <a href="." className="block text-white ">
+        RETRY
+      </a>
+    </div>
+  );
+}
